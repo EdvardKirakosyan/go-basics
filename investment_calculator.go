df5bbab0d40c484b038cefe621a562831a4b0f07
Edvard Kirakosyan <kirakosyan3010@gmail.com>
2025-03-16 19:43:09 +0400

    c

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+func main() {
+	var investmentAmount = 1000
+	var expectedReturnRate = 5.5
+	var years = 10
+
+	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+
+	fmt.Println(futureValue)
 }
