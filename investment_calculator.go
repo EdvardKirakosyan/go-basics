0ebff52a3bd9b262bbd95e563ac719f57659d13c
Edvard Kirakosyan <kirakosyan3010@gmail.com>
2025-03-16 21:36:43 +0400

    c

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,11 +6,16 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	const inflationRate = 6.5
+	var investmentAmount float64
+	years := 10.0
+	expectedReturnRate := 5.5
 
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	fmt.Scan(&investmentAmount)
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
