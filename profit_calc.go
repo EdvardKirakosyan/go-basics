06f78a466d9b57cd3f8bdc4f675e9d98fdc77f0f
Edvard Kirakosyan <kirakosyan3010@gmail.com>
2025-03-17 17:34:37 +0400

    с

--- profit_calc.go
+++ profit_calc.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var revenue int
-	var expenses int
+	var revenue float64
+	var expenses float64
 	var taxRate float64
 
 	fmt.Print("Revenue: ")
@@ -13,8 +13,19 @@ func main() {
 	fmt.Print("Expenses: ")
 	fmt.Scan(&expenses)
 
-	fmt.Print("taxRate: ")
+	fmt.Print("Tax Rate: ")
 	fmt.Scan(&taxRate)
 
 	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+
+	ratio := ebt / profit
+
+	// fmt.Println("Ebt: ", ebt)
+	// fmt.Println("Profit: ", profit)
+	// fmt.Println("Ratio", ratio)
+
+	fmt.Printf("Ebt: %v\n", ebt)
+	fmt.Printf("Profit: %v\n", profit)
+	fmt.Printf("Ratio: %v\n", ratio)
 }
