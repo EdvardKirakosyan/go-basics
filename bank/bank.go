921ad827db2ab6214d9682f8a357cbc49a10ed45
Edvard Kirakosyan <e.kirakosyan.andersenlab@gmail.com>
2025-03-18 14:16:13 +0400

    c

---
+++ bank/bank.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to GO bank")
+	fmt.Println("What do you want to do")
+	fmt.Println("1. Check balance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("2. Withdraw money")
+	fmt.Println("2. EXIT")
+
+}
