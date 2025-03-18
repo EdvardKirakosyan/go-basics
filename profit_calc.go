332edab614082aa84a772698662cf293fc32cc21
Edvard Kirakosyan <e.kirakosyan.andersenlab@gmail.com>
2025-03-18 12:14:23 +0400

    c

--- profit_calc.go
+++ profit_calc.go
@@ -5,9 +5,10 @@ import (
 	"math"
 )
 
+const inflationRate = 6.5
+
 func main() {
-	const inflationRate = 6.5
-	var investmentAmount float64
+	var investmentAmount float64 = 5.5
 	var years float64
 	expectedReturnRate := 5.5
 
@@ -31,4 +32,10 @@ func main() {
 
 func outputText(text string) {
 	fmt.Print(text)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) {
+	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv := fv / math.Pow(1+inflationRate/100, years)
+	return fv, rfv
 }
